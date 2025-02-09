da718fd23f9ae78f4576fb364de2268d68d9af93
Joshua Grey <greyj3@cardiff.ac.uk>
2025-02-09 17:12:14 +0000

    feat/Clicking on nav bar links redirects

--- frontend/CHIL/src/components/marginals/public-header.tsx
+++ frontend/CHIL/src/components/marginals/public-header.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Toolbar from '@mui/material/Toolbar';
-import LogoDev from '@mui/icons-material/LogoDev';
-
-const navItems = ["Home", "Regions", "Data", "Newsletter", "About Us", "Contact"]
+import { publicHeaderConfig } from '../../config/headerLinks';
+import { useNavigate } from 'react-router-dom';
+import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { LogoDev } from '@mui/icons-material';
 
 const PublicHeader = (): React.JSX.Element => {
+
+    const navigate = useNavigate();
+
     return (
         <AppBar position="absolute">
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
                 <LogoDev/>
 
                 <Box>
-                    {navItems.map((item) => (
-                    <Button color='inherit' key={item}>
-                        {item}
+                    {publicHeaderConfig.map((item) => (
+                    <Button color='inherit' key={item[0]} onClick={() => {navigate(item[1])}}>
+                        {item[0]}
                     </Button>
                     ))}
 
-                    <Button color='inherit' variant='outlined' sx={{ml: 2}}>
+                    <Button
+                        color='inherit'
+                        variant='outlined'
+                        sx={{ml: 2}}
+                        onClick={() => {navigate('/login')}}
+                    >
                         Login
                     </Button>
                 </Box>
