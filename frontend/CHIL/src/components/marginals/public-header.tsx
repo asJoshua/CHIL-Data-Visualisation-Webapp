0859acd5ce0eda010229939b1e0e459104fed127
Joshua Grey <greyj3@cardiff.ac.uk>
2025-02-09 16:52:49 +0000

    feat/created nav bar

--- frontend/CHIL/src/components/marginals/public-header.tsx
+++ frontend/CHIL/src/components/marginals/public-header.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import LogoDev from '@mui/icons-material/LogoDev';
 
+const navItems = ["Home", "Regions", "Data", "Newsletter", "About Us", "Contact"]
 
 const PublicHeader = (): React.JSX.Element => {
     return (
-        <>
-            Hello
-        </>
+        <AppBar position="absolute">
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <LogoDev/>
+
+                <Box>
+                    {navItems.map((item) => (
+                    <Button color='inherit' key={item}>
+                        {item}
+                    </Button>
+                    ))}
+
+                    <Button color='inherit' variant='outlined' sx={{ml: 2}}>
+                        Login
+                    </Button>
+                </Box>
+
+            </Toolbar>
+        </AppBar>
     )
 }
 
