7c6847e327f33ab04a45ebbbda619eb38b11ca1a
Joshua Grey <greyj3@cardiff.ac.uk>
2025-02-12 13:23:31 +0000

    fix/Added path alias to vite

---
+++ frontend/CHIL/src/app/routes/public/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PublicLayout } from '@components/layouts/public-layout';
+
+const LoginRoot = (): React.JSX.Element => {
+    return (
+        <PublicLayout />
+    )
+}
+
+export { LoginRoot }
--- frontend/CHIL/vite.config.ts
+++ frontend/CHIL/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        '@components': '/src/components',
+        '@config': '/src/config',
+        '@marginals': '/src/components/marginals',
+        '@routes': '/src/app',
+    }
+  }
 })
