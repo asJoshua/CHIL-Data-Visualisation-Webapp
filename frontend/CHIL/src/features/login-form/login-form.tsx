34dcd618fc2d51b473ad67c8e48bd17ab95611a7
Joshua Grey <greyj3@cardiff.ac.uk>
2025-02-13 15:03:48 +0000

    feat/Login form submits to function

--- frontend/CHIL/src/features/login-form/login-form.tsx
+++ frontend/CHIL/src/features/login-form/login-form.tsx
@@ -1,32 +1,59 @@
+import { use, useState } from 'react';
 import { Box } from '@/components/ui/box/box';
 import { TextField } from '@/components/ui/text-field/text-field';
 import { Button } from '@/components/ui/button/button';
 
 const LoginForm = () => {
+    const [ showError, setError ] = useState(false)
+    const [ usernameErrorMessage, setUsernameErrorMessage ] = useState("")
+    const [ passwordErrorMessage, setPasswordErrorMessage ] = useState("")
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+        console.log("Username: ", username);
+        console.log("Password: ", password);
+    };
+
     return (
         <Box
-            className='flex flex-col gap-y-4 flex-wrap justify-center'
-            sx={{
-                width: '20vw',
-                height: '50vh',
-            }}
+            sx={{width: '20vw', height: '50vh'}}
         >
-            <h1 className='text-3xl font-bold text-center'>LOGIN</h1>
-            <TextField
-                variant='outlined'
-                label='Username'
-            />
-            <TextField
-                variant='outlined'
-                label='Password'
-            />
-            <Button
-                variant='contained'
-                size='small'
-                color='primary'
-            >
-                Login
-            </Button>
+            <form className='flex flex-col gap-y-4 flex-wrap justify-center'>
+                <h1 className='text-3xl font-bold text-center'>LOGIN</h1>
+                <TextField
+                    variant='outlined'
+                    label='Username'
+                    error={showError}
+                    helperText={showError ? usernameErrorMessage : "" }
+                    onChange={handleUsernameChange}
+                    />
+                <TextField
+                    variant='outlined'
+                    label='Password'
+                    type='password'
+                    error={showError}
+                    helperText={showError ? passwordErrorMessage : "" }
+                    onChange={handlePasswordChange}
+                    />
+                <Button
+                    variant='contained'
+                    size='small'
+                    color='primary'
+                    onClick={handleLogin}
+                    >
+                    Login
+                </Button>
+            </form>
         </Box>
     );
 };
