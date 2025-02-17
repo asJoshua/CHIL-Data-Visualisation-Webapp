3eda5576d0b44c9e4698bcf715e3c5e382c8a386
George Bridle <bridlegb@cardiff.ac.uk>
2025-02-17 13:13:06 +0000

    test/added frontend tests for deployments page

---
+++ frontend/CHIL/__test__/features/deployments-page.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { DeploymentsPage } from '../../src/features/deployments'
+import '@testing-library/jest-dom/jest-globals';
+import '@testing-library/jest-dom';
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as object),
+  useNavigate: () => mockNavigate,
+}));
+
+describe('Deployments Page', () => {
+    beforeEach(() => {
+        mockNavigate.mockClear();
+        global.alert = jest.fn();
+    });
+
+    it('renders the deployments page correctly', () => {
+        render(
+            <MemoryRouter>
+                <DeploymentsPage />
+            </MemoryRouter>
+        );
+
+        // Check table component and search bar are present
+        expect(screen.getByRole("heading", {name: "Deployments"})).toBeInTheDocument();
+        expect(screen.getByPlaceholderText("Search Deployments...")).toBeInTheDocument();
+    });
+
+    it("shows 'No data available' when there are no rows", () => {
+        render(
+            <MemoryRouter>
+                <DeploymentsPage />
+            </MemoryRouter>
+        );
+
+        // Simulate searching for something that doesn't exist
+        fireEvent.change(screen.getByPlaceholderText("Search Deployments..."), {
+            target: { value: "NonExistentDeployment" },
+        });
+
+        // Expect "No data available" to be shown
+        expect(screen.getByText("No data available")).toBeInTheDocument();
+    });
+})
