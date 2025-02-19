8120495a73e9d877a47bf49f2b4f086f91f4b5c6
George Bridle <bridlegb@cardiff.ac.uk>
2025-02-19 21:49:44 +0000

    fix/on load screen positioning

--- frontend/CHIL/src/features/deployments.tsx
+++ frontend/CHIL/src/features/deployments.tsx
@@ -18,6 +18,8 @@ const rows = [
     { name: "Deployment 1", description: "Test description", startDate: "2023-01-01", endDate: "2023-12-31", campaignId: "1234" },
     { name: "Deployment 2", description: "Another description", startDate: "2023-05-01", endDate: "2023-11-30", campaignId: "5678" },
     { name: "Deployment 3", description: "Test description", startDate: "2023-01-01", endDate: "2023-12-31", campaignId: "1234" },
+    { name: "Deployment 1", description: "Test description", startDate: "2023-01-01", endDate: "2023-12-31", campaignId: "1234" },
+    { name: "Deployment 2", description: "Another description", startDate: "2023-05-01", endDate: "2023-11-30", campaignId: "5678" },
 ];
 
 const DeploymentsPage = (): React.JSX.Element => {
@@ -32,7 +34,7 @@ const DeploymentsPage = (): React.JSX.Element => {
 
 // to set the page to the top when it loads, was being funky
     useEffect(() => {
-        window.scrollTo(0, 0);
+        window.scrollTo(0, 40);
     }, []);
 
     const handleRowClick = (id: string) => {
@@ -42,7 +44,7 @@ const DeploymentsPage = (): React.JSX.Element => {
     return (
         <CustomThemeProvider>
                 <Container className="flex justify-center items-center min-h-screen px-24 pb-20">
-                    <Container className="flex flex-col items-start w-full max-w-full">
+                    <Container className="flex flex-col items-start w-full max-w-full pt-12">
                         <Typography variant="h2" color="primary.main">Deployments</Typography>
                         <Typography variant="h4" color="secondary.main">All deployments of our instruments</Typography>
                         <div style={{paddingTop: "5px", paddingBottom: "5px"}}>
