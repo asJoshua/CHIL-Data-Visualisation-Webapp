64af72fc256ce4fea9567bf721650c0a7d19e0c0
Joshua Grey <greyj3@cardiff.ac.uk>
2025-02-19 11:00:36 +0000

    fix/Flake8 compliance

--- backend/CHIL/chil/auth/urls.py
+++ backend/CHIL/chil/auth/urls.py
@@ -10,7 +10,19 @@ from .views import (
 )
 
 urlpatterns = [
-    path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', LogoutView.as_view(), name="logout"),
+    path(
+        'token/',
+        CookieTokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/',
+        CookieTokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name="logout"
+    ),
 ]
