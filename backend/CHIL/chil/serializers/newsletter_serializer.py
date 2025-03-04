b18d5dc7cb676fc3daff59ac3b9aeff96a99936d
George Bridle <bridlegb@cardiff.ac.uk>
2025-03-04 16:48:30 +0000

    feat/email sends, and pylint passes

--- backend/CHIL/chil/serializers/newsletter_serializer.py
+++ backend/CHIL/chil/serializers/newsletter_serializer.py
@@ -1,13 +1,21 @@
+"""
+The serializer for Newsletter signup
+model: Specifies that this serializer is associated with the `Subscribers` model.
+fields: Lists the fields to be serialized. In this case, it includes only the `email` field.
+"""
+
 from rest_framework import serializers
 from ..models.newsletter_model import Subscribers
 
+# pylint: disable=too-few-public-methods
 class NewsletterSignupSerializer(serializers.ModelSerializer):
+    """
+    Serializer for handling newsletter subscription sign-ups.
+    """
     class Meta:
+        """
+        Meta class to configure the behavior of the NewsletterSignupSerializer.
+        Specifies the model to use (Subscribers) and the fields to include (email).
+        """
         model = Subscribers
-        fields = ["email", "subscribed_at"]
-
-    def validate_email(self, value):
-        """Check if email is valid and not already subscribed"""
-        if Subscribers.objects.using('newsletterdb').filter(email=value).exists():
-            raise serializers.ValidationError("This email is already subscribed.")
-        return value
+        fields = ["email"]
--- backend/CHIL/chil/urls.py
+++ backend/CHIL/chil/urls.py
@@ -5,5 +5,6 @@ before being got by the top level URL file in config/urls
 from django.urls import include, path
 
 urlpatterns = [
-    path('newsletter/', include('chil.all_urls.newsletter-url'))
+    path('auth/', include('chil.all_urls.auth')),
+    path('newsletter/', include('chil.all_urls.newsletter'))
 ]
