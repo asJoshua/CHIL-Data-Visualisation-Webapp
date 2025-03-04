9de066df9c9ddbf728c40e210fa708c30178c81c
George Bridle <bridlegb@cardiff.ac.uk>
2025-03-04 11:18:28 +0000

    feat/email sends

--- backend/CHIL/chil/urls.py
+++ backend/CHIL/chil/urls.py
@@ -2,9 +2,8 @@
 Top level URL file for the chil project. Each feature's url's are collated here
 before being got by the top level URL file in config/urls
 """
-
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth/', include('chil.all_urls.auth'))
+    path('newsletter/', include('chil.all_urls.newsletter-url'))
 ]
--- backend/CHIL/schema/newsletter-schema.sql
+++ backend/CHIL/schema/newsletter-schema.sql
@@ -4,7 +4,7 @@ BEGIN;
 -- Create newsletter table
 ------------------------------------------------------------------------------
 
-CREATE TABLE newsletterSubscribers (
+CREATE TABLE newsletter_subscribers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     subscribed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
