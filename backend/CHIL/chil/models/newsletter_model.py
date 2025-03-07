04c3e831041f8cafee313c634a8365a703feb051
George Bridle <bridlegb@cardiff.ac.uk>
2025-03-07 16:11:22 +0000

    chore/fixing linting, adding docstrings

--- backend/CHIL/chil/models/newsletter_model.py
+++ backend/CHIL/chil/models/newsletter_model.py
@@ -1,12 +1,21 @@
+"""
+Defines the susbscribers model
+"""
+
 from django.db import models
 
 class Subscribers(models.Model):
+    """
+    Represents a subscriber
+    """
+
     # unique ensurs no duplicate entries
     email = models.EmailField(unique=True)
     subscribed_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
+    class Meta: #pylint: disable=too-few-public-methods
+        """Overide settings"""
         db_table = 'newsletter_subscribers'
 
     def __str__(self):
-        return self.email
+        return str(self.email)
