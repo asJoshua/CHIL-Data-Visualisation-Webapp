f85875b7f4c94f741d1569d7c048cb1c5df2a3b3
George Bridle <bridlegb@cardiff.ac.uk>
2025-03-07 17:23:00 +0000

    fix/pylint fix

--- backend/CHIL/chil/tests/test_newsletter_view.py
+++ backend/CHIL/chil/tests/test_newsletter_view.py
@@ -40,7 +40,7 @@ class NewsletterSignupTestCase(TestCase):
         self.assertEqual(response.data['error'], "This email is already subscribed")
 
     @patch('chil.models.newsletter_model.Subscribers.objects.using')
-    def test_newsletter_signup_no_email(self, mock_db):
+    def test_newsletter_signup_no_email(self, _mock_db):
         """
         400 error is returned when no email is provided.
         """
