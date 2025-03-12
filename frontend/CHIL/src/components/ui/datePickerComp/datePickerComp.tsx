50b3ab1343e39423e93afcff7aabd93d29f8ffa7
c22050719 <kendrewo@cardiff.ac.uk>
2025-03-12 20:22:05 +0000

    feat/removed uneeded import

--- frontend/CHIL/src/components/ui/datePickerComp/datePickerComp.tsx
+++ frontend/CHIL/src/components/ui/datePickerComp/datePickerComp.tsx
@@ -3,7 +3,6 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; 
 import styles from './datePickerComp.module.css'; 
 import './datePickerOverride.css'; 
-import { Box } from '@mui/material';
 
 export const DatePickerComp = (props: {
     id: string | undefined; 
