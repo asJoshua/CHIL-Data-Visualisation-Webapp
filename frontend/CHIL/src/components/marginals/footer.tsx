d3e2f521fa8628c9dc8fb85deca165da3576e99e
asJoshua <joshuaandrewsadleir@gmail.com>
2025-03-23 15:52:42 +0100

    feat/social-media-icons

--- frontend/CHIL/src/components/marginals/footer.tsx
+++ frontend/CHIL/src/components/marginals/footer.tsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { footerConfig } from "@/config/footerLinks";
 import { ThemeProvider } from "@mui/material/styles";
-import { BottomNavigation} from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { BottomNavigation, IconButton} from "@mui/material";
 import { Box } from "@/components/ui/box/box"
-import { Button } from "@/components/ui/button/button"
 import { theme } from "@/theme/theme";
 import logo from '@/assets/images/CHIL-Logo.svg'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Footer = (): React.JSX.Element => {
-  const navigate = useNavigate();
 
   return (
     <ThemeProvider theme={theme}>
       <Box>
         <BottomNavigation sx={{padding: theme.spacing(2) ,display: 'flex', justifyContent: 'space-between', backgroundColor:theme.palette.secondary.main}}>
-          <img src={logo} alt="CHIL Logo" style={{ height: 25 }} />
-          <Box>
-            {footerConfig.map((item) => (
-              <Button sx={{color: theme.palette.text.primary, fontWeight: 300}} key={item[0]} onClick={() => {navigate(item[1])}}>
-              {item[0]}
-          </Button>
-            ))}
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <IconButton href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" sx={{ color: "#E4405F" }}>
+              <InstagramIcon sx={{color: 'white'}}/>
+            </IconButton>
+            <IconButton href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" sx={{ color: "#0077B5" }}>
+              <LinkedInIcon sx={{color: 'white'}}/>
+            </IconButton>
+            <IconButton href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" sx={{ color: "#1877F2" }}>
+              <FacebookIcon sx={{color: 'white'}}/>
+            </IconButton>
           </Box>
+          <img src={logo} alt="CHIL Logo" style={{ height: 25 }} />
         </BottomNavigation>
       </Box>
     </ThemeProvider>
