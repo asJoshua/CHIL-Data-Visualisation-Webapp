3f6ba319ef3469a1bae888ca92a7bb95363433a1
c22050719 <kendrewo@cardiff.ac.uk>
2025-03-29 17:30:46 +0000

    feat/reverting to old commit

--- frontend/CHIL/src/components/ui/lineGraph/lineGraph.stories.tsx
+++ frontend/CHIL/src/components/ui/lineGraph/lineGraph.stories.tsx
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { LineGraph } from './lineGraph';
-import { createDataset } from './datasetObject';
+// import type { Meta, StoryObj } from '@storybook/react';
+// import { LineGraph } from './lineGraph';
+// import { createDataset } from './datasetObject';
 
-const exampleDataset = createDataset(
-  'Axis One',
-  [1, 5, 6, 2],
-  'rgb(255, 99, 132)',
-  'rgb(255, 99, 132)'
-)
+// const exampleDataset = createDataset(
+//   'Axis One',
+//   [1, 5, 6, 2],
+//   'rgb(255, 99, 132)',
+//   'rgb(255, 99, 132)'
+// )
 
-const meta = {
-  component: LineGraph,
-} satisfies Meta<typeof LineGraph>;
+// const meta = {
+//   component: LineGraph,
+// } satisfies Meta<typeof LineGraph>;
 
-export default meta;
+// export default meta;
 
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    titleText: "ExmapleTitle",
-    datasets: [exampleDataset],
-    labels: ['A', 'B', 'C', 'D']
-  }
-};
+// export const Default: Story = {
+//   args: {
+//     titleText: "ExmapleTitle",
+//     datasets: [exampleDataset],
+//     labels: ['A', 'B', 'C', 'D']
+//   }
+// };
