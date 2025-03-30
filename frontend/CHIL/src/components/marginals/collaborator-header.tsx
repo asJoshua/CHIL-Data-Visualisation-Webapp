25170c1ff238a49947e0f94d6b7dcc48fd9b9a39
asJoshua <joshuaandrewsadleir@gmail.com>
2025-03-31 00:53:30 +0100

    feat/auth-user-header-hover

--- frontend/CHIL/src/components/marginals/collaborator-header.tsx
+++ frontend/CHIL/src/components/marginals/collaborator-header.tsx
@@ -22,15 +22,27 @@ const CollaboratorHeader = ({imageSrc}:CollaboratorHeaderProps): React.JSX.Eleme
                 <img src={imageSrc} alt="CHIL Logo" style={{ height: 25 }} />
                 <Box>
                     {collaboratorHeaderConfig.map((item) => (
-                        <Button sx={{color: theme.palette.text.primary, fontWeight: 300}} key={item[0]} onClick={() => {navigate(item[1])}}>
+                        <Button sx={{
+                            color: theme.palette.text.primary, 
+                            fontWeight: 300, 
+                            transition: 'color 0.3s ease-in-out',
+                            '&:hover': { color: theme.palette.secondary.main } 
+                            }} key={item[0]} onClick={() => {navigate(item[1])}}>
                             {item[0]}
                         </Button>
                     ))}
 
                     <Button
-                        color='inherit'
-                        variant='outlined'
-                        sx={{ml: 2}}
+                        sx={{
+                            ml: 2, 
+                            fontWeight: 300,
+                            backgroundColor: theme.palette.primary.light, 
+                            color: theme.palette.text.primary,
+                            transition: 'background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
+                            '&:hover': { 
+                                backgroundColor: theme.palette.secondary.main,
+                                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' 
+                            }}}
                         onClick={() => {
                             logout('chil/auth/logout/', setToken, navigate)
                         }}
